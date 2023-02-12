ce66b32ddd4f8392cf856df9ddc3686afa645e6f
TomomiTsukazaki <122886012+tsukazaki-t@users.noreply.github.com>
2023-02-12 22:23:46 +0900

    課題提出

---
+++ src/For01.java
@@ -0,0 +1,13 @@
+
+public class For01 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+		for(int number = 1; number <= 5; number++) {
+			System.out.println("For01 = " + number);
+		}
+
+	}
+
+}
